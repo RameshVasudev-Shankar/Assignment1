243b71344b8ff92e151d8b047a5e8e399bdc0eb4
Shankar Ramesh <shankar.rameshvasudev@my.jcu.edu.au>
2016-04-11 20:37:31 +1000

    Psedudocode with load_all_item() function and a bit of the main menu

--- Ramesh Vasudev Shankar A1.py
+++ Ramesh Vasudev Shankar A1.py
@@ -28,47 +28,55 @@
 #
 # '''
 #
-#
-# def main():
-#
-#     print("\t Items for Hire - by Shankar Ramesh  Vasudev")
-#     MENU = "\t Menu: \n\t (L)ist all items \n\t (H)ire an item\n\t (Return) an item\n\t (A)dd new items to stock\n\t (Q)uit"
-#     print(MENU)
-#     choice = input(">>> ").upper()
-#     while choice != 'Q':
-#         if choice == 'L':
-#             list_all_item()
-#         elif choice == 'H':
-#             hire_an_item()
-#         elif choice == 'R':
-#             return_an_item()
-#         elif choice == 'A':
-#             add_item()
-#         else:
-#             print("Invalid menu choice")
-#         print(MENU)
-#         choice = input(">>> ").upper()
-#     print("Farewell Message") #change it - just for reference
-#
 
-def load_all_item():
+def main():
+
+    print("\t Items for Hire - by Shankar Ramesh  Vasudev")
+    MENU = "Menu: \n(L)ist all items\n(H)ire an item\n(R)eturn an item\n(A)dd new items to stock\n(Q)uit"
+    items = load_item()
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != 'Q':
+        if choice == 'L':
+            list_all_item(items)
+        if choice == 'H':
+            hire_an_item(items)
+        print(MENU)
+        choice = input(">>> ").upper()
+        #  save_item()
+    print("Farewell Message")  # change it - just for reference
+
+
+def load_item():
+    file = open('items.csv')
+    items = []
+    for line in file:
+        item_in_list = line.strip().split(',')
+        item_in_list[2] = float(item_in_list[2])
+        items.append(item_in_list)
+    file.close()
+    return items
+
+
+def list_all_item(items):
     print("All items on file ( '*' indicates item is currently out):")
-    file = open('items.csv') #reading the csv file
-    file_read = file.readlines() # reading all the lines of the csv file one by one
-    for line in range(len(file_read)):
-        item_in_list = file_read[line].split(',')
-        item_name = item_in_list[0]
-        item_desc = item_in_list[1]
-        item_cost = item_in_list[2]
-        item_position = item_in_list[3].rstrip()
+    for number in range(len(items)):
+
+        item_name = items[number][0]
+        item_desc = items[number][1]
+        item_cost = items[number][2]
+        item_position = items[number][3]
         if item_position == 'out':
-            item_sign = "*"
+             item_sign = "*"
         else:
             item_sign = ""
-        print("{} - {} ({}) = $ {} {}".format(line, item_name, item_desc, item_cost, item_sign))
-    file.close()
-load_all_item()
 
-def hire_an_item():
-    
+        print("{} - {:20} {:30} = $ {:>4.2f} {}".format(number, item_name, '('+item_desc+')', item_cost, item_sign))
 
+# load_all_item()
+
+
+def hire_an_item(items):
+
+
+main()
